324b9d68eab63693a2d99a8d96b615ff2fd40704
Sayanika2001 <108542048+Sayanika2001@users.noreply.github.com>
2022-07-28 11:15:49 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    function printDiv(divName) {
+        var printContents = document.getElementById(divName).innerHTML;
+        w=window.open();
+        w.document.write(printContents);
+        w.print();
+        w.close();
+		
+    }
+</script>
